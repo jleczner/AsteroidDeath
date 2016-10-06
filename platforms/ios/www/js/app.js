041ad61459b98b852132ca461182a318da7945ee
Jonathan Leczner <jonathanleczner@Jonathan-ZCW-16.local>
2016-10-05 22:26:26 -0400

    deleted duplicate folder

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -9,18 +9,44 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+	// Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+	// for form inputs)
+	if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+	  cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	  cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
+	}
+	if (window.StatusBar) {
+	  // org.apache.cordova.statusbar required
+	  StatusBar.styleDefault();
+	}
   });
+})
+
+.controller('ToDoController', function($scope) {
+	$scope.todos = [];
+	$scope.todoModel = {};
+	$scope.todoModel.todo = '';
+	$scope.addTodo = function() {
+		$scope.todos.push($scope.todoModel.todo);
+		$scope.todoModel = {
+			todo: ''
+		}
+	};
+})
+
+.directive('ngEnter', function () {
+    return function ($scope, element, attrs) {
+        element.bind("keydown keypress", function (event) {
+            if(event.which === 13) {
+                $scope.$apply(function (){
+                    $scope.$eval(attrs.ngEnter);
+                });
+ 
+                event.preventDefault();
+            }
+        });
+    };
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
@@ -32,51 +58,51 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
   $stateProvider
 
   // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
+	.state('tab', {
+	url: '/tab',
+	abstract: true,
+	templateUrl: 'templates/tabs.html'
   })
 
   // Each tab has its own nav history stack:
 
   .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
+	url: '/dash',
+	views: {
+	  'tab-dash': {
+		templateUrl: 'templates/tab-dash.html',
+		controller: 'DashCtrl'
+	  }
+	}
   })
 
   .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
-        }
-      }
-    })
+	  url: '/chats',
+	  views: {
+		'tab-chats': {
+		  templateUrl: 'templates/tab-chats.html',
+		  controller: 'ChatsCtrl'
+		}
+	  }
+	})
+	.state('tab.chat-detail', {
+	  url: '/chats/:chatId',
+	  views: {
+		'tab-chats': {
+		  templateUrl: 'templates/chat-detail.html',
+		  controller: 'ChatDetailCtrl'
+		}
+	  }
+	})
 
   .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
+	url: '/account',
+	views: {
+	  'tab-account': {
+		templateUrl: 'templates/tab-account.html',
+		controller: 'AccountCtrl'
+	  }
+	}
   });
 
   // if none of the above states are matched, use this as the fallback
