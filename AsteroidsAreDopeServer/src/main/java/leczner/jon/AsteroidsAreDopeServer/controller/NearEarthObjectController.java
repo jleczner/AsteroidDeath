ff30a5d494d0fbf02e3d687e068885244436e759
Jonathan Leczner <jleczner@udel.edu>
2016-11-04 11:58:13 -0400

    added config and setup datasources

--- AsteroidsAreDopeServer/src/main/java/leczner/jon/AsteroidsAreDopeServer/controller/NearEarthObjectController.java
+++ AsteroidsAreDopeServer/src/main/java/leczner/jon/AsteroidsAreDopeServer/controller/NearEarthObjectController.java
@@ -3,14 +3,18 @@ package leczner.jon.AsteroidsAreDopeServer.controller;
 import leczner.jon.AsteroidsAreDopeServer.model.NearEarthObject;
 import leczner.jon.AsteroidsAreDopeServer.model.NearEarthObjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
 /**
  * Created by jonathanleczner on 10/16/16.
  */
-@RestController
+@Controller
 @RequestMapping("api/v1/")
 public class NearEarthObjectController {
 
